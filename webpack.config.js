e39c6617c902f520d93d3e1558eb4f3cb99bf13a
Siddharth VP <siddharthvp@gmail.com>
2021-05-16 09:05:07 +0530

    webpack.config.js: don't use file caching
    More trouble than it is worth. The performance benefits are very marginal. But preventing updates of css from showing up by default seems undesirable.

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,10 @@ const args = require('minimist')(process.argv.slice(2));
 
 const corePath = args.core || './node_modules/twinkle-core';
 
-let fileCache = {};
-
-// For the css files and twinkle-core files, don't read from file system on
-// every page load. Rather load them just once, if they change the server
-// needs to be restarted for the changes to take effect. Use --nocache argument
-// to actually cause the files to be read from filesystem every time - this may
-// slow down things. This is not applicable for the typescript sources which
-// are always refreshed on every code change.
-function readFile(file) {
-	if (fileCache[file] && !args.nocache) {
-		return fileCache[file];
-	}
-	return (fileCache[file] = fs.readFileSync(file).toString());
-}
-
 module.exports = {
 	mode: 'development',
 	devtool: 'source-map',
+
 	entry: './src/twinkle.ts',
 	target: ['web', 'es5'],
 	module: {
@@ -42,6 +28,7 @@ module.exports = {
 		filename: 'twinkle.js',
 		path: path.resolve(__dirname, 'build'),
 	},
+
 	devServer: {
 		before: function (app, server, compiler) {
 			app.get('/core/*', function (req, response) {
@@ -67,3 +54,7 @@ module.exports = {
 		port: 5500,
 	},
 };
+
+function readFile(file) {
+	return fs.readFileSync(file).toString();
+}
